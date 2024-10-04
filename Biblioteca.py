149bf501aa7abc9c0b6e23b243d8eb0802e5a7bc
Henrique Walace <henriquewalace@hotmail.com>
2024-10-04 15:14:05 -0300

    Cria header com imagem e titulo

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,7 +1,21 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 JanelaLogin = Tk()
 
 JanelaLogin.title("Login")
+
+frame = Frame(JanelaLogin)
+frame.pack(padx=20,pady=20)
+
+image = Image.open("img/Brasão.png")
+image = image.resize((50,50))
+img = ImageTk.PhotoImage(image)
+
+img_label = Label(frame,image=img)
+img_label.pack(side="left",padx=10)
+
+texto_Label = Label(frame,text="Biblioteca Erico Verissimo", font=("Open Sans", 20))
+texto_Label.pack(side="left")
 
 JanelaLogin.mainloop()
