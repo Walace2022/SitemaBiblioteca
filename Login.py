e46b46acdb5d45aab71561f3da9c8bb419daacc0
Henrique Walace <henriquewalace@hotmail.com>
2024-10-07 09:57:27 -0300

    Atualiza para formato de navegação

--- Login.py
+++ Login.py
@@ -1,42 +1,45 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
-JanelaLogin = Tk()
 
-JanelaLogin.title("Login")
-JanelaLogin.geometry("430x600")
+class Login(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        
+        frame = Frame(self)
+        frame.pack(padx=20,pady=20)
+        
+     
 
-frame = Frame(JanelaLogin)
-frame.pack(padx=20,pady=20)
+        self.image = Image.open("img/Brasão.png")
+        self.image = self.image.resize((50,50))
+        self.img = ImageTk.PhotoImage(self.image)
+        
+        img_label = Label(frame,image=self.img)
+        img_label.pack(side="left",padx=10)
 
-image = Image.open("img/Brasão.png")
-image = image.resize((50,50))
-img = ImageTk.PhotoImage(image)
-
-img_label = Label(frame,image=img)
-img_label.pack(side="left",padx=10)
-
-texto_Label = Label(frame,text="Biblioteca Erico Verissimo", font=("Open Sans", 20))
-texto_Label.pack(side="left")
-
-
-frame_form = Frame(JanelaLogin, bg="lightgray", padx=20, pady=20)
-frame_form.pack(padx=20, pady=20, fill="x")
+        texto_Label = Label(frame,text="Biblioteca Erico Verissimo", font=("Open Sans", 20))
+        texto_Label.pack(side="left")
 
 
-cpf_label = Label(frame_form, text="CPF:", bg="lightgray")
-cpf_label.grid(row=0, column=0, sticky="w", pady=5)
-cpf_entry = Entry(frame_form)
-cpf_entry.grid(row=0, column=1, pady=5)
+        frame_form = Frame(self, bg="lightgray", padx=20, pady=20)
+        frame_form.pack(padx=20, pady=20, fill="x")
+
+        frame_form.grid_columnconfigure(1, weight=1)
+
+        cpf_label = Label(frame_form, text="CPF:", bg="lightgray")
+        cpf_label.grid(row=0, column=0, sticky="w", pady=5)
+        cpf_entry = Entry(frame_form)
+        cpf_entry.grid(row=0,sticky="we", column=1, pady=5)
 
  
-password_label = Label(frame_form, text="Senha:", bg="lightgray")
-password_label.grid(row=1, column=0, sticky="w", pady=5)
-password_entry = Entry(frame_form, show="*")
-password_entry.grid(row=1, column=1, pady=5)
+        password_label = Label(frame_form, text="Senha:", bg="lightgray")
+        password_label.grid(row=1, column=0, sticky="w", pady=5)
+        password_entry = Entry(frame_form, show="*")
+        password_entry.grid(row=1,sticky="we", column=1, pady=5)
 
    
-login_button = Button(frame_form, text="Login", command=lambda: print("Login clicado!"))
-login_button.grid(row=2, columnspan=2, pady=10)
+        login_button = Button(frame_form, text="Login", command=lambda: controller.mostrar_frame("Consulta"))
+        login_button.grid(row=2, columnspan=2, pady=10)
 
-JanelaLogin.mainloop()
+        
